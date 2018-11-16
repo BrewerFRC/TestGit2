e38b018dfeb2def01f7426ed90853da73d1b14ed
Qwertyness <mccoythedev@gmail.com>
2018-11-15 20:40:04 -0500

    Random whitespace.

--- src/org/usfirst/frc/team4564/robot/Robot.java
+++ src/org/usfirst/frc/team4564/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+	
 
 	@Override
 	public void robotInit() {
